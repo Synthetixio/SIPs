ab6afa94eb6e5e21651eb6e7575cfbc809e4a0fb
carrotanderson <134579337+carrotanderson@users.noreply.github.com>
2023-06-30 15:39:05 +1000

    Include STPs in title (#1614)
    * Include STPs in title
    
    * Update stp-{MarkdownRemark.frontmatter__stp}.tsx

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   siteMetadata: {
     siteUrl: 'https://www.yourdomain.tld',
-    title: 'Synthetix SIPs & SCCPs',
+    title: 'Synthetix SIPs, STPs & SCCPs',
   },
   plugins: [
     'gatsby-plugin-typescript',
